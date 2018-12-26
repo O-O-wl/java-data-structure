bb2296cdb3f1dc09ba1f41f36fb4b8da32fc47fb
dongY <ldcpaul@hanmail.net>
2018-12-26 13:00:37 +0900

    버블 정렬

---
+++ Chap06_정렬/버블정렬.java
@@ -0,0 +1,27 @@
+package Do_It_자료구조.Chap06_정렬;
+
+public class 버블정렬 {
+    // 인접한 두요소를 정렬
+    public static void bubbleSort(int[]arr){
+        for(int i=0;i<arr.length-1;i++){
+            int temp;
+            if(arr[i]>arr[i+1])
+            {
+                temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+                i=0;
+                continue;
+            }
+        }
+    }
+
+
+    public static void main(String arg[]){
+        int[]arr = {1,2,3,4,23,9,11,100};
+        bubbleSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+", ");
+        }
+    }
+}
